66be186ea653875b9d8e7ebb4f52bbbb0d37b133
Mr. Tetteh <danielstay73@gmail.com>
2024-11-24 23:56:49 +0000

    settings

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::get('/all_posts', [PostController::class, 'index'])->middleware(Cors::class);
     Route::get('/public_profile/{id}', [PostController::class, 'pub_profile'])->middleware(Cors::class);
     Route::get('/user_posts', [PostController::class, 'user_posts'])->middleware(Cors::class);
-    Route::post('/post', [PostController::class, 'store'])->middleware(Cors::class);
+    Route::post('/post', [PostController::class, 'storee'])->middleware(Cors::class);
     Route::get('/users/{id}', [UserController::class, 'user']);
     Route::post('/logout', [UserController::class, 'logout']);
 
